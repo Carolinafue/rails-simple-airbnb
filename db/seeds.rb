bc6eda84c99aaf9fc018a4ae6bc73ff2f13b93ed
carolina fuentes <fuecarolina@gmail.com>
2021-08-04 18:31:51 -0400

    agregar columna

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.destroy_all
+
+
+4.times do
+  Flat.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.word,
+    price_per_night: Faker::Number.decimal(l_digits: 3, r_digits: 3),
+    number_of_guests: Faker::Number.decimal_part(digits: 2))
+end
